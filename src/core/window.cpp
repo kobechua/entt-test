fe255dace6620294d7c11cc68360e8c4d95b27cd
Kobe <kobechua14@gmail.com>
2025-11-30 20:18:51 -0500

    weekend work

---
+++ src/core/window.cpp
@@ -0,0 +1,226 @@
+#include "window.hpp"
+
+Window::Window(const char *title, int width, int height) : backgroundColor(glm::vec4(0, 0, 0, 1))
+{
+    m_Title = title;
+    m_Width = (float)width;
+    m_Height = (float)height;
+    if(!init())
+        glfwTerminate();
+
+    for(int i = 0; i < MAX_KEYS ; i++)
+    {
+        m_HeldKeys[i] = false;
+		m_PressedKeys[i] = false;
+		m_ReleasedKeys[i] = false;
+    }
+    for(int i = 0; i < MAX_BUTTONS ; i++)
+    {
+        m_HeldMouseButtons[i] = false;
+    }
+
+    firstMouse = true;
+    lastMouseX = width / 2;
+    lastMouseY = height / 2;
+}
+
+Window::~Window()
+{
+    glfwTerminate();
+}
+
+bool Window::init()
+{
+    glfwSetErrorCallback(glfw_initialisation_error);
+    if(!glfwInit())
+        return false;
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+#endif
+
+    glfwWindowHint(GLFW_SAMPLES, 16);
+
+    m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+    if(!m_Window)
+    {
+        glfwTerminate();
+        std::cerr << "ERROR::GLFW::Could not initialise GLFW Window" << '\n';
+        return false;
+    }
+    glfwMakeContextCurrent(m_Window);
+
+    glfwSetWindowUserPointer(m_Window, this);
+
+    // Window Callback Functions
+    glfwSetFramebufferSizeCallback(m_Window, window_resize_callback);
+    glfwSetKeyCallback(m_Window, key_callback);
+    glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
+    glfwSetCursorPosCallback(m_Window, mouse_position_callback);
+
+    // initialize GLAD using glfw
+    int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    // Print out some info about the graphics drivers
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+
+    glEnable(GL_BLEND);
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    return true;
+}
+
+bool Window::isKeyPressed(unsigned int keycode)
+{
+	if (keycode >= MAX_KEYS)
+		return false;
+
+    bool result = m_PressedKeys[keycode];
+    m_PressedKeys[keycode] = false;
+
+    return result;
+}
+
+bool Window::isKeyReleased(unsigned int keycode)
+{
+	if (keycode >= MAX_KEYS)
+		return false;
+
+	bool result = m_ReleasedKeys[keycode];
+    m_ReleasedKeys[keycode] = false;
+
+    return result;
+}
+
+bool Window::isKeyHeld(unsigned int keycode) const
+{
+	// TODO: Log this
+	if (keycode >= MAX_KEYS)
+		return false;
+
+	return m_HeldKeys[keycode];
+}
+
+bool Window::isMouseButtonPressed(unsigned int button)
+{
+	if (button >= MAX_BUTTONS)
+		return false;
+
+	bool result = m_PressedMouseButtons[button];
+    m_PressedMouseButtons[button] = false;
+
+	return result;
+}
+
+bool Window::isMouseButtonReleased(unsigned int button)
+{
+	if (button >= MAX_BUTTONS)
+		return false;
+
+	bool result = m_ReleasedMouseButtons[button];
+    m_ReleasedMouseButtons[button] = false;
+
+	return result;
+}
+
+bool Window::isMouseButtonHeld(unsigned int button) const
+{
+	// TODO: Log this
+	if (button >= MAX_BUTTONS)
+		return false;
+
+	return m_HeldMouseButtons[button];
+}
+
+void Window::getMousePosition(double& x, double& y) const
+{
+    x = m_MouseX;
+    y = m_MouseY;
+}
+
+void Window::clear() const
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glClearColor(backgroundColor.x, backgroundColor.y, backgroundColor.z, backgroundColor.w);
+}
+
+void Window::update() const
+{
+    double currentTime = glfwGetTime(); deltaTime = currentTime - m_LastTime; m_LastTime = currentTime;
+
+    glfwPollEvents();
+    glfwSwapBuffers(m_Window);
+}
+
+bool Window::closed() const
+{
+    return glfwWindowShouldClose(m_Window) == 1;
+}
+
+void glfw_initialisation_error(int error, const char* description)
+{
+    std::cerr << "ERROR::GLFW::" << error << "::DESCRIPTION::" << description << std::endl;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Window* wind = (Window *) glfwGetWindowUserPointer(window);
+	wind->m_ReleasedKeys[key] = ((action == GLFW_RELEASE) && wind->m_HeldKeys[key]);
+    wind->m_HeldKeys[key] = action != GLFW_RELEASE;
+    if(!wind->m_PressedKeys[key])
+        wind->m_PressedKeys[key] = (action == GLFW_PRESS);
+
+
+    // Window closing
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        std::cout << "Escape Key Pressed..." << "Quiting..." <<'\n';
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void window_resize_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, (float)width, (float)height);
+    Window* wind = (Window *) glfwGetWindowUserPointer(window);
+    wind->m_Width = (float)width;
+    wind->m_Height = (float)height;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    Window* wind = (Window *) glfwGetWindowUserPointer(window);
+	wind->m_ReleasedMouseButtons[button] = ((action == GLFW_RELEASE) && wind->m_ReleasedMouseButtons[button]);
+    wind->m_HeldMouseButtons[button] = action != GLFW_RELEASE;
+	if (!wind->m_PressedMouseButtons[button])
+	    wind->m_PressedMouseButtons[button] = action == GLFW_PRESS;
+}
+
+void mouse_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    Window* wind = (Window *) glfwGetWindowUserPointer(window);
+    wind->m_MouseX = xpos;
+    wind->m_MouseY = ypos;
+
+    if (wind->firstMouse)
+    {
+        wind->lastMouseX = xpos;
+        wind->lastMouseY = ypos;
+        wind->firstMouse = false;
+    }
+    wind->deltaMouseX = xpos - wind->lastMouseX;
+    wind->deltaMouseY = wind->lastMouseY - ypos;
+
+    wind->lastMouseX = xpos;
+    wind->lastMouseY = ypos;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 //EXTERNAL LIBRARIES
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 #include <entt/entt.hpp>
+#include <stb_image.h>
+
 
 //LOCAL LIBRARIES
 #include "gfx/shader.h"
+#include "core/window.hpp"
 
 //STANDARD
 #include <iostream>
@@ -16,8 +21,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "../res/shaders/shader.vert";
 const char *fragmentShaderSource = "../res/shaders/shader.frag";
@@ -26,121 +31,53 @@ const char *fragmentShaderSource = "../res/shaders/shader.frag";
 int main()
 {
 
-   std::cout << std::filesystem:: current_path() << "\n";
+    Window window = Window("Kobe", SCR_WIDTH, SCR_HEIGHT);
+    
 
-   // glfw: initialize and configure
-   // ------------------------------
-   glfwInit();
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-   // glfw window creation
-   // --------------------
-   GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-   if (window == NULL)
-   {
-      std::cout << "Failed to create GLFW window" << std::endl;
-      glfwTerminate();
-      return -1;
-   }
-   glfwMakeContextCurrent(window);
-   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-   // glad: load all OpenGL function pointers
-   // ---------------------------------------
-   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-   {
-      std::cout << "Failed to initialize GLAD" << std::endl;
-      return -1;
-   }
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+        0.5f, -0.5f, 0.0f, // right 
+        0.0f,  0.5f, 0.0f  // top   
+    }; 
 
 
-   // set up vertex data (and buffer(s)) and configure vertex attributes
-   // ------------------------------------------------------------------
-   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // left  
-      0.5f, -0.5f, 0.0f, // right 
-      0.0f,  0.5f, 0.0f  // top   
-   }; 
+    Shader shader = Shader(vertexShaderSource, fragmentShaderSource);
 
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
 
-   Shader shader = Shader(vertexShaderSource, fragmentShaderSource);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-   unsigned int VBO, VAO;
-   glGenVertexArrays(1, &VAO);
-   glGenBuffers(1, &VBO);
-   glBindVertexArray(VAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-   glEnableVertexAttribArray(0);
+    glBindVertexArray(0); 
 
-   glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-   glBindVertexArray(0); 
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window.getGLFWwindow()))
+    {
+        window.clear();
 
-   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        shader.use();
+        glBindVertexArray(VAO); 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-   // render loop
-   // -----------
-   while (!glfwWindowShouldClose(window))
-   {
-      // input
-      // -----
-      processInput(window);
+        window.update();
+    }
 
-      // render
-      // ------
-      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
-      // draw our first triangle
-      shader.use();
-      glBindVertexArray(VAO); 
-      glDrawArrays(GL_TRIANGLES, 0, 3);
-
-      // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-      // -------------------------------------------------------------------------------
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-   }
-
-   // optional: de-allocate all resources once they've outlived their purpose:
-   // ------------------------------------------------------------------------
-   glDeleteVertexArrays(1, &VAO);
-   glDeleteBuffers(1, &VBO);
-
-   // glfw: terminate, clearing all previously allocated GLFW resources.
-   // ------------------------------------------------------------------
-   glfwTerminate();
-   return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(window, true);
-
-   if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-   {
-      std::cout << "W Pressed" << "\n";
-   }
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-   // make sure the viewport matches the new window dimensions; note that width and 
-   // height will be significantly larger than specified on retina displays.
-   glViewport(0, 0, width, height);
+    glfwTerminate();
+    return 0;
 }
